78d0c64833ad57d0de84ac18d16fa6d511714415
Krish Patel <krishpatel6666@gmail.com>
2024-10-24 14:39:41 -0400

    Updated zombies

--- Pair Project 2/Assets/Scripts/Enemy/EnemyCollision.cs
+++ Pair Project 2/Assets/Scripts/Enemy/EnemyCollision.cs
@@ -3,17 +3,17 @@ using UnityEngine.SceneManagement; // For reloading the scene
 
 public class EnemyCollision : MonoBehaviour
 {
+    public GameObject player; 
     // When something enters the enemy's trigger collider
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider col)
     {
         // Check if the object that entered the trigger is the player
-        if (other.CompareTag("Player"))
+        if (col.CompareTag("Player"))
         {
             // Option 1: Reload the current scene to reset everything
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-
             // Option 2: Instead of reloading, reset the player’s position manually
-            // other.transform.position = new Vector3(0, 1, 0); // Example reset position
+            // col.transform.position = new Vector3(0, 1, 0); // Example reset position
         }
     }
 }
